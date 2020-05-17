dca37faa18ee5dec992067943708f35cf98cf4cd
lukas <dr.oste129@gmail.com>
2020-05-17 16:14:04 +0200

    setup vue

---
+++ src/router/index.js
@@ -0,0 +1,8 @@
+import StartScreen from '../components/StartScreen'
+
+export const routes = [
+    // Startseite
+    {
+        path: '/', component: StartScreen, meta: { transitionName: 'slide'}
+    },
+];
